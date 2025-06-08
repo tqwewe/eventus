374a8725fead9744b2d15c1f6c3b5a7d5d25bce1
Ari Seyhun <dev@tqwewe.com>
2025-06-08 14:44:33 +1000

    chore: conditionally add darwin specific packages

--- flake.nix
+++ flake.nix
@@ -2,17 +2,28 @@
   description = "Dev shell";
 
   inputs = {
-    nixpkgs.url      = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     rust-overlay.url = "github:oxalica/rust-overlay";
-    flake-utils.url  = "github:numtide/flake-utils";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
+  outputs =
+    {
+      nixpkgs,
+      rust-overlay,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
       let
         overlays = [ (import rust-overlay) ];
         rust-bin = pkgs.rust-bin.stable.latest.default.override {
-          extensions = [ "rust-src" "rustfmt" "rust-analyzer" ];
+          extensions = [
+            "rust-src"
+            "rustfmt"
+            "rust-analyzer"
+          ];
         };
         pkgs = import nixpkgs {
           inherit system overlays;
@@ -22,19 +33,26 @@
       with pkgs;
       {
         devShells.default = mkShell {
-          buildInputs = [
-            cargo-flamegraph
-            openssl
-            pkg-config
-            protobuf
-            libiconv
-            gcc
-            rust-bin
-            darwin.apple_sdk.frameworks.CoreServices
-            darwin.apple_sdk.frameworks.CoreFoundation
-            darwin.apple_sdk.frameworks.Security
-            darwin.apple_sdk.frameworks.SystemConfiguration
-          ];
+          buildInputs =
+            [
+              cargo-flamegraph
+              openssl
+              pkg-config
+              protobuf
+              libiconv
+              gcc
+              rust-bin
+            ]
+            ++ pkgs.lib.optionals pkgs.stdenv.isDarwin (
+              with pkgs.darwin.apple_sdk.frameworks;
+              [
+                CoreServices
+                CoreFoundation
+                Security
+                SystemConfiguration
+              ]
+            );
+
         };
       }
     );
